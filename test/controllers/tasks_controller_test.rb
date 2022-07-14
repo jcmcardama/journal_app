d5d2c8f9bc1fdffbf713324e79ab7285b0e2b7c6
Carlo <jcmcardama@gmail.com>
2022-07-14 20:56:02 +0800

    add test and design

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -4,4 +4,18 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should show task" do
+    @category = Category.create(name: "Personal")
+    @task = Task.create(category_id: @category.id, name: "Personal")
+
+    get category_task_path(@category.id, @task.id)
+    assert_response :success
+  end
+
+  test "should create new task" do
+    @category = Category.create(name: "Personal")
+
+    post category_tasks_path(@category.id), params: {"task": { "name": "Personal Task" , "category_id": "1"}}
+    assert_response :redirect
+  end
 end
